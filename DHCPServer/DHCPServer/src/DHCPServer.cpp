130b0c61c4c457d88569a9335f893d7ffd0a1596
Surojit Sengupta <surojit.sengupta@sjsu.edu>
2014-11-07 15:10:32 -0800

    Initial Commit - Base File

---
+++ DHCPServer/DHCPServer/src/DHCPServer.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DHCPServer.cpp
+// Author      : Surojit Sengupta
+// Version     :
+// Copyright   : All Rights Reserved 2014
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
