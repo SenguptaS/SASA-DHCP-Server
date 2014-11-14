cb2636d65ed2084a97ddcba37998f2283e737175
ateiv jain <ateiv.jain@gmail.com>
2014-11-14 11:37:43 -0800

    First attempt- defined variables...please give me feedback !!

--- DHCPServer/DHCPServer/src/DHCPServer.cpp
+++ DHCPServer/DHCPServer/src/DHCPServer.cpp
@@ -9,7 +9,81 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
-}
+//Discover Packet
+struct DiscoverPacket{
+	char mOpField;
+	char mHeaderType;
+	unsigned int mHeaderLength;
+	unsigned int mHops;
+	unsigned int mTransactionId;
+	unsigned int mSeconds;
+	char mFlags;
+	unsigned int mClientAddress;
+	unsigned int mYourAddress;
+	unsigned int mServerAddress;
+	unsigned int mGatewayAddress;
+	unsigned long int mClientHardwareAddress;
+	char mServerName;
+	char mBootFileName;
+	char mOptions;
+
+};
+
+//Request Packet
+struct RequestPacket{
+	char mOpField;
+	char mHeaderType;
+	unsigned int mHeaderLength;
+	unsigned int mHops;
+	unsigned int mTransactionId;
+	unsigned int mSeconds;
+	char mFlags;
+	unsigned int mClientAddress;
+	unsigned int mYourAddress;
+	unsigned int mServerAddress;
+	unsigned int mGatewayAddress;
+	unsigned long int mClientHardwareAddress;
+	char mServerName;
+	char mBootFileName;
+	char mOptions;
+
+};
+
+//Response Packet
+struct ResponsePacket{
+	char mOpField;
+	char mHeaderType;
+	unsigned int mHeaderLength;
+	unsigned int mHops;
+	unsigned int mTransactionId;
+	unsigned int mSeconds;
+	char mFlags;
+	unsigned int mClientAddress;
+	unsigned int mYourAddress;
+	unsigned int mServerAddress;
+	unsigned int mGatewayAddress;
+	unsigned long int mClientHardwareAddress;
+	char mServerName;
+	char mBootFileName;
+	char mOptions;
+};
+
+//Acknowledgment Packet
+struct AcknowledgmentPacket{
+	char mOpField;
+	char mHeaderType;
+	unsigned int mHeaderLength;
+	unsigned int mHops;
+	unsigned int mTransactionId;
+	unsigned int mSeconds;
+	char mFlags;
+	unsigned int mClientAddress;
+	unsigned int mYourAddress;
+	unsigned int mServerAddress;
+	unsigned int mGatewayAddress;
+	unsigned long int mClientHardwareAddress;
+	char mServerName;
+	char mBootFileName;
+	char mOptions;
+};
+
