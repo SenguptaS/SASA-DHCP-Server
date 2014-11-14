0505b01d9ef6c382cad203b24ef0136b7be4ddaf
Surojit Sengupta <surojit.sengupta@sjsu.edu>
2014-11-14 14:23:59 -0800

    Added logging subsystem to the DHCP Server

--- DHCPServer/DHCPServer/src/DHCPServer.cpp
+++ DHCPServer/DHCPServer/src/DHCPServer.cpp
@@ -7,9 +7,16 @@
 //============================================================================
 
 #include <iostream>
+#include <log4cxx/logger.h>
+#include <log4cxx/propertyconfigurator.h>
+
 using namespace std;
 
 int main() {
+	log4cxx::PropertyConfigurator::configure("config/log.cfg");
+	log4cxx::LoggerPtr pLogger = log4cxx::Logger::getLogger("SASA-DHCPSERVER");
+	time_t mTimeNow = time(NULL);
+	LOG4CXX_INFO(pLogger,"SASA DHCP Server Starting up - " << ctime(&mTimeNow));
 
 	return 0;
 }
