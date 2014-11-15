7625a820c4a0076e01a2aa15d316b96e99d565d2
aditi-rajawat <aditi.rajawat@sjsu.edu>
2014-11-14 17:27:37 -0800

    Changes in databaseConnections committed.

--- IPPoolServer/IPPoolServer/src/databaseConnection.h
+++ IPPoolServer/IPPoolServer/src/databaseConnection.h
@@ -10,6 +10,8 @@
 
 #include "IPPoolServerConstants.h"
 #include <mysql++/connection.h>
+#include <mysql++/query.h>
+#include <mysql++/result.h>
 #include <log4cxx/logger.h>
 #include <iostream>
 #include <iomanip>
@@ -25,7 +27,9 @@ class databaseConnection{
 public:
 
 	databaseConnection();
-	void createConnection();
+	int fireQuery();
+	void setMQuery(String lQuery);
+	StoreQueryResult getMResult();
 
 private:
 	LoggerPtr mPLogger;
@@ -35,6 +39,10 @@ private:
 	string mDatabase;
 	string mPassword;
 	string mServer;
+	String mQuery;
+	StoreQueryResult mResult;
+
+	int createConnection();
 
 };
 
@@ -47,15 +55,52 @@ databaseConnection::databaseConnection(){
 	port = 0;
 }
 
-void databaseConnection::createConnection(){
+int databaseConnection::createConnection(){
 
 	if(mConn.connect(mDatabase.c_str(), mServer.c_str(), mUsrername.c_str(), mPassword.c_str(), port)){
 		LOG4CXX_INFO(mPLogger,"Connection with IP Pool database successfully established..");
+		return 0;
 	}
 	else{
 		LOG4CXX_ERROR(mPLogger, "Connection with IP Pool could not be established at..");
+		return -1;
 	}
 
+}
+
+int databaseConnection::fireQuery(){
+
+	if(!createConnection()){
+		if((mQuery.is_null()) || (mQuery.size()<1)){
+			LOG4CXX_ERROR(mPLogger, "Query to the database is null..could not retrieve results for null query");
+			return -1;
+		}
+		else{
+			Query lQuery = mConn.query(mQuery.c_str());
+			LOG4CXX_INFO(mPLogger,"Query to the database set as "<< mQuery);
+			if(this->mResult = lQuery.store()){
+				LOG4CXX_INFO(mPLogger, "Data successfully retrieved from the database..");
+				return 0;
+			}
+			else{
+				LOG4CXX_ERROR(mPLogger, "Something seems wrong with the query or database...unsuccessful query execution..");
+				return -1;
+			}
+
+		}
+	}
+	else{
+		LOG4CXX_ERROR(mPLogger,"Connection to the database is unestablished..");
+		return -1;
+	}
+}
+
+void databaseConnection::setMQuery(const String lQuery){
+	this->mQuery = lQuery;
+}
+
+StoreQueryResult databaseConnection::getMResult(){
+	return mResult;
 }
 
 
