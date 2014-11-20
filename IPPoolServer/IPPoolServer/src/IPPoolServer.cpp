9195a4f2b9394ed5653c88e8506a83ae8ed7f033
Surojit Sengupta <surojit.sengupta@sjsu.edu>
2014-11-20 12:32:35 -0800

    Added configuration reading mechanism

--- IPPoolServer/IPPoolServer/src/IPPoolServer.cpp
+++ IPPoolServer/IPPoolServer/src/IPPoolServer.cpp
@@ -18,10 +18,12 @@
 #include <unistd.h>
 #include <cerrno>
 #include <errno.h>
+#include "Settings.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
+
 	int lSocketFd, lNewSocketFd, lPortNo, lNoOfCharRead;
 	char lbuffer[BUFFER_SIZE];
 	struct sockaddr_in lServerAddr;
@@ -31,10 +33,40 @@ int main() {
 	log4cxx::PropertyConfigurator::configure("log.cfg");
 	log4cxx::LoggerPtr pLogger = log4cxx::Logger::getLogger(ROOT_LOGGER);
 	time_t mTimeNow = time(NULL);
-	LOG4CXX_INFO(pLogger,"IPPool Starting up - " << ctime(&mTimeNow));
+
+	if (argc < 3) {
+		LOG4CXX_FATAL(pLogger,
+				"No configuration file provided. Usage: IPPoolServer -c <config_file>");
+		return EXIT_FAILURE;
+	}
+
+	if (strcmp(argv[1], "-c") != 0) {
+		LOG4CXX_FATAL(pLogger, "Invalid option " << argv[1]);
+		return EXIT_FAILURE;
+	}
+
+	if (strlen(argv[2]) <= 0) {
+		LOG4CXX_FATAL(pLogger,"Invalid or No configuration file provided");
+		return EXIT_FAILURE;
+	}
+
+	Settings lSettings(argv[2]);
+	try
+	{
+		lSettings.ReadSettings();
+	}
+	catch(std::exception &ex)
+	{
+		LOG4CXX_FATAL(pLogger,"Failed to read settings file - " << ex.what());
+		return EXIT_FAILURE;
+	}
+
+	LOG4CXX_INFO(pLogger, "Read settings file - " << argv[2]);
+
+	LOG4CXX_INFO(pLogger, "IPPool Starting up - " << ctime(&mTimeNow));
 
 	lSocketFd = socket(AF_INET, SOCK_STREAM, 0);
-	if(lSocketFd <0){
+	if (lSocketFd < 0) {
 		LOG4CXX_ERROR(pLogger, "Error opening the socket");
 	}
 
@@ -44,24 +76,26 @@ int main() {
 	lServerAddr.sin_port = htons(65500);
 	lServerAddr.sin_addr.s_addr = 0;
 
-	if(bind(lSocketFd, (struct sockaddr* )&lServerAddr, sizeof(lServerAddr))){
-		LOG4CXX_ERROR(pLogger, "Socket binding failed...try some other port no.");
-	}
-	else{
+	if (bind(lSocketFd, (struct sockaddr*) &lServerAddr, sizeof(lServerAddr))) {
+		LOG4CXX_ERROR(pLogger,
+				"Socket binding failed...try some other port no.");
+	} else {
 		listen(lSocketFd, 5);
-		lNewSocketFd = accept(lSocketFd, (struct sockaddr*) &lClientAddr, &lClientAddressSize);
-		if(lNewSocketFd <0){
-			LOG4CXX_ERROR(pLogger, "Failed to establish a connection with the client - " << strerror(errno));
-		}
-		else{
-			LOG4CXX_INFO(pLogger, "Successfully established a connection with the client..");
+		lNewSocketFd = accept(lSocketFd, (struct sockaddr*) &lClientAddr,
+				&lClientAddressSize);
+		if (lNewSocketFd < 0) {
+			LOG4CXX_ERROR(pLogger,
+					"Failed to establish a connection with the client - " << strerror(errno));
+		} else {
+			LOG4CXX_INFO(pLogger,
+					"Successfully established a connection with the client..");
 			bzero(lbuffer, BUFFER_SIZE);
 			lNoOfCharRead = read(lNewSocketFd, lbuffer, BUFFER_SIZE);
-			if(lNoOfCharRead <0){
+			if (lNoOfCharRead < 0) {
 				LOG4CXX_ERROR(pLogger, "Error reading from the socket");
-			}
-			else{
-				LOG4CXX_INFO(pLogger,"Message read from the socket -->" << lbuffer);
+			} else {
+				LOG4CXX_INFO(pLogger,
+						"Message read from the socket -->" << lbuffer);
 			}
 		}
 		close(lSocketFd);
---
+++ IPPoolServer/IPPoolServer/src/Settings.cpp
@@ -0,0 +1,52 @@
+/*
+ * Settings.cpp
+ *
+ *  Created on: Nov 18, 2014
+ *      Author: surojit
+ */
+
+#include "Settings.h"
+#include <fstream>
+
+Settings::Settings(std::string lSettingsFile) {
+	this->mServerPort = 3306;
+	mSettingsFile = lSettingsFile;
+}
+
+Settings::~Settings() {
+
+}
+
+void Settings::ReadSettings() {
+	std::ifstream lFile;
+	std::string lLine;
+	lFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try {
+		lFile.open(mSettingsFile.c_str());
+		std::string delim = ",";
+
+		std::getline(lFile, lLine, ',');
+		this->mDBUser = lLine;
+
+		std::getline(lFile, lLine, ',');
+		this->mDBPassword = lLine;
+
+		std::getline(lFile, lLine, ',');
+		this->mDBServer = lLine;
+
+		std::getline(lFile, lLine, ',');
+		this->mDBPort = strtoul(lLine.c_str(), NULL, 10);
+
+		std::getline(lFile, lLine, ',');
+		this->mDBSchema = lLine;
+
+		std::getline(lFile, lLine, ',');
+		this->mServerPort = strtoul(lLine.c_str(), NULL, 10);
+		lFile.close();
+		return;
+	} catch (std::exception &e) {
+		throw std::runtime_error(e.what());
+	}
+	return;
+}
+
