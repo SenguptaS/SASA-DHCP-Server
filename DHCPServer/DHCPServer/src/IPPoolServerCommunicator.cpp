50b4a1e41fd0109fe910ae3127836e38c7859572
Sumit <sumit.rindhe@sjsu.edu>
2014-11-20 11:45:16 -0800

    socket made

--- DHCPServer/DHCPServer/src/IPPoolServerCommunicator.cpp
+++ DHCPServer/DHCPServer/src/IPPoolServerCommunicator.cpp
@@ -13,6 +13,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <log4cxx/logger.h>
+#include <log4cxx/propertyconfigurator.h>
+#include <netdb.h>
 
 IPPoolServerCommunicator::IPPoolServerCommunicator(std::string lServerIPAddress,
 		int lServerPort, unsigned short lServerIdentifer) {
@@ -33,7 +37,44 @@ int IPPoolServerCommunicator::getIpLease(std::string mac,
 
 	RequestPacketPS r;
 	in_addr addr;
-	inet_aton(previousIp.c_str(),&addr);
+	inet_aton(previouRequestPacketPS rsIp.c_str(),&addr);
+	memcpy(r.mSourceHardwareAddress,mac.c_str(),6);
+	 r.mOpField=1;
+	r.mRequestId = transactionId;
+	r.mPreviousIP = addr.s_addr;
+	r.mChecksum = 0;
+	r.mServerId = mServerIdentifier;
+	r.mProtocolType = 4;
+
+
+	int s = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in remoteSocketInfo;
+	struct hostent *hPtr;
+	const char *remoteHost="localhost";
+	hPtr = gethostbyname(remoteHost);
+	memcpy((char *)&remoteSocketInfo.sin_addr,0,0);
+	remoteSocketInfo.sin_family = AF_INET;
+	remoteSocketInfo.sin_port = htons((u_short)68);
+	int connect(socket, (struct sockaddr *)&remoteSocketInfo, sizeof(sockaddr_in));
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+int IPPoolServerCommunicator::confirmIp(std::string mac, std::string ip) {
+
+	RequestPacketPS r;
+	in_addr addr;
+	inet_aton(previouRequestPacketPS rsIp.c_str(),&addr);
 	memcpy(r.mSourceHardwareAddress,mac.c_str(),6);
 	 r.mOpField=1;
 	r.mRequestId = transactionId;
@@ -47,16 +88,23 @@ int IPPoolServerCommunicator::getIpLease(std::string mac,
 }
 
 
-int IPPoolServerCommunicator::confirmIp(std::string mac, std::string ip) {
-
-
-	return 0;
-}
-
 
 int IPPoolServerCommunicator::releaseIp(std::string mac, std::string ip) {
 
 
+	RequestPacketPS r;
+	in_addr addr;
+	inet_aton(previouRequestPacketPS rsIp.c_str(),&addr);
+	memcpy(r.mSourceHardwareAddress,mac.c_str(),6);
+	 r.mOpField=1;
+	r.mRequestId = transactionId;
+	r.mPreviousIP = addr.s_addr;
+	r.mChecksum = 0;
+	r.mServerId = mServerIdentifier;
+	r.mProtocolType = 4;
+
 	return 0;
 
 }
+
+
